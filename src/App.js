3a923ad67bf393db9f9cf947e2e3c74184e07631
md umar faruk <mdumar11233>
2021-06-24 10:24:59 +0600

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+            
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
